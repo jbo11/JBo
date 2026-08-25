a7ed622210fe2a6edcd7b878f909fb414592e80a
jbo11 <expose.pinas@gmail.com>
2026-08-25 16:21:01 -0700

    Update 082526-3

--- analytics/client/analytics-config.js
+++ analytics/client/analytics-config.js
@@ -1,6 +1,6 @@
 window.JB_ANALYTICS_CONFIG = {
   enabled: true,
-  endpoint: "https://script.google.com/macros/s/AKfycbxuyKTwBbM4_hS3J7MXM6a5uGnvR0ZkVKBKxosgl3nOPxR_1EApdoeaBXRhQ7rh9O3iyA/exec",
+  endpoint: "https://script.google.com/macros/s/AKfycbxyVbxe8u-kF573HGNufe8gzjXdSF88oK1wabDfo9V0OfJbdInBSx9smtmyUv_oV-_j/exec",
   heartbeatInterval: 60000,
   onlineThreshold: 120000,
   debug: false
